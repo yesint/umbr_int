085c75a60e145693d53bb90126788cf13302d57d
yesint <yesint4@yahoo.com>
2025-07-05 20:49:51 +0200

    Fixed common_time intervals logic, added sanity checks for enum values

--- umbr_int.py
+++ umbr_int.py
@@ -4,12 +4,14 @@ import tomllib
 import sys
 from pathlib import Path
 
-sqrt_two_pi = math.sqrt(math.tau)
-kb = 0.0083144621  # Boltzmann constant in kJ/(mol*K)
+SQRT_TWO_PI = math.sqrt(math.tau)
+KB = 0.0083144621  # Boltzmann constant in kJ/(mol*K)
+INTERVALS_TYPES = ['window_time','common_time']
+ZERO_POINT_TYPES = ['left','right','min']
 
 "Single umbrella window"
 class Window:
-    def __init__(self, pos, k, filename, first_time):
+    def __init__(self, pos, k, filename, begin):
         self.pos = pos
         self.k = k
 
@@ -27,12 +29,12 @@ class Window:
                 continue
             
             t = float(fields[0])
-            if t>=first_time:
+            if t>=begin:
                 time.append(t)
                 data.append(float(fields[1]))
 
         if len(data) == 0:
-            raise Exception('No values read. first_time is too large or file is empty.')
+            raise Exception('No values read. `begin` is too large or file is empty.')
         
         self.time = np.array(time, dtype=float)
         self.data = np.array(data, dtype=float)
@@ -40,7 +42,7 @@ class Window:
         print(f'\t{len(self.data)} values, t={self.time[0]}..{self.time[-1]}')
 
 
-    def interval_mean_std(self, i: int):
+    def interval_mean_std(self, i: int|None):
         span = self.intervals[i] if i!=None else self.data[:]    
         self.mean = np.mean(span)
         self.std = np.std(span)
@@ -49,13 +51,16 @@ class Window:
 
 class Config:
     def __init__(self, toml_file: str):
+        print(f'Reading input file `{inp_file}`...')
         toml = tomllib.load(open(toml_file, 'rb'))
-        self.first_time = toml.get('first_time',-1.0)
+        print(f'There are {len(toml['windows'])} umbrella windows')
+        
+        self.begin = toml.get('begin',-1.0)
         
         self.windows = []
         for w in toml['windows']:
-            self.windows.append(Window(w['pos'], w['k'], w['file'], self.first_time))
-
+            self.windows.append(Window(w['pos'], w['k'], w['file'], w.get('begin',self.begin)))
+        
         # Convert angular force constant if asked
         if toml.get('k_rad2_to_deg2',False):
             print('Convertin angular force constants: kJ mol^-1 rad^-2 --> kJ mol^-1 deg^-2')
@@ -65,29 +70,34 @@ class Config:
 
         self.Nintervals = toml.get('Nintervals',1)
         intervals_type = toml.get('intervals_type','window_time')
+
+        # Sanity check
+        if intervals_type not in INTERVALS_TYPES:
+            raise Exception(f'Intervals type `{intervals_type}` is not recognized. Supported: {INTERVALS_TYPES}')
         
         if self.Nintervals>1:
             if intervals_type == 'common_time':
                 print('Defining intervals by common time of all windows')
                 # Find common time interval for all windows
-                min_t = np.min([w.time[0] for w in self.windows])
-                max_t = np.max([w.time[-1] for w in self.windows])
+                min_t = np.max([w.time[0] for w in self.windows])
+                max_t = np.min([w.time[-1] for w in self.windows])
 
                 if max_t - min_t <= 0:
                     raise Exception(f"Windows don't cover a common time interval!")
 
-                print(f'Windows cover common time interval {min_t}..{max_t}')
-                t_intervals = np.linspace(min_t,max_t,self.Nintervals)
+                print(f'\tWindows cover common time interval {min_t}..{max_t}')
+                t_intervals = np.linspace(min_t,max_t,self.Nintervals+2)[1:-1]
                 
                 # Set index intervals for windows
                 for w in self.windows:
                     split_points = []
                     for ti in t_intervals:
-                        while w.time[cur] < ti[0]:
-                            cur += 1
-                        split_points.append(cur)
+                        c = 0
+                        while w.time[c] < ti:
+                            c += 1
+                        split_points.append(c)
                     # This returns array views so no data is copied
-                    w.intervals = np.array_split(w.data, split_points)[1:-1]
+                    w.intervals = np.array_split(w.data, split_points)
             else:
                 print('Defining intervals independently for each window')
                 for w in self.windows:
@@ -104,14 +114,18 @@ class Config:
         
         self.out_file = toml.get('out_file','pmf.dat')
         self.kt_units = toml.get('kt_units',False)
-        self.zero_point = toml.get('zero_point','left')
-        
+        self.zero_point = toml.get('zero_point','min')
+
+        # Sanity check
+        if self.zero_point not in ZERO_POINT_TYPES:
+            raise Exception(f'Zero point type `{self.zero_point}` is not recognized. Supported: {ZERO_POINT_TYPES}')
+         
         self.Nbin = toml.get('Nbin',100)
         self.bin_sz = (self.bin_max - self.bin_min) / self.Nbin
 
 
 def Pb(x: float, mean: float, std: float) -> float:
-    return 1.0 / (std * sqrt_two_pi) * np.exp(-0.5 * ((x - mean) / std)**2)
+    return 1.0 / (std * SQRT_TWO_PI) * np.exp(-0.5 * ((x - mean) / std)**2)
 
 
 def compute_interval(config: Config, interval: int|None) -> np.ndarray:
@@ -120,7 +134,7 @@ def compute_interval(config: Config, interval: int|None) -> np.ndarray:
     else:
         print(f'Computing whole PMF...')
 
-    kbT = kb * config.Temperature    
+    kbT = KB * config.Temperature    
 
     dAu = np.zeros((config.Nwin, config.Nbin))
     dAfinal = np.zeros(config.Nbin)
@@ -172,33 +186,41 @@ def write_pmf(outfile: str, pmf: np.ndarray, config: Config, errors: np.ndarray|
 ####################
 # Run
 ####################
-config = Config(sys.argv[1])
+if __name__ == "__main__":
+    print('====================================')
+    print(' Umbrella Integration unbiasing     ')
+    print(' (C) Semen Yesylevskyy, 2025        ')
+    print(' https://github.com/yesint/umbr_int ')
+    print('====================================')
 
-# Compute whole PMF
-whole_pmf = compute_interval(config,None)
+    inp_file = sys.argv[1]
+    config = Config(inp_file)
 
-pmfs = np.zeros((config.Nbin, config.Nintervals))
-rmsd_pmfs = np.zeros((config.Nbin, config.Nintervals))
+    # Compute whole PMF
+    whole_pmf = compute_interval(config,None)
 
-for i in range(config.Nintervals):
-    pmfs[:,i] = compute_interval(config,i)
-    
-    # Align to the whole pmf
-    optimal_c = np.mean(whole_pmf - pmfs[:,i])
-    rmsd_pmfs[:,i] = pmfs[:,i] + optimal_c
-    
-    # Writing individual intervals
-    stem = Path(config.out_file).stem
-    ext = Path(config.out_file).suffix
-    write_pmf(f'{stem}_{i}{ext}', rmsd_pmfs[:,i], config, errors=None)
+    pmfs = np.zeros((config.Nbin, config.Nintervals))
+    rmsd_pmfs = np.zeros((config.Nbin, config.Nintervals))
 
-# Write whole PMF
-if config.Nintervals>1:
-    # Compute errors in each point
-    errs = np.zeros(config.Nbin)
-    for i in range(config.Nbin):    
-        errs[i] = np.std(rmsd_pmfs[i,:])
+    for i in range(config.Nintervals):
+        pmfs[:,i] = compute_interval(config,i)
+        
+        # Align to the whole pmf
+        optimal_c = np.mean(whole_pmf - pmfs[:,i])
+        rmsd_pmfs[:,i] = pmfs[:,i] + optimal_c
+        
+        # Writing individual intervals
+        stem = Path(config.out_file).stem
+        ext = Path(config.out_file).suffix
+        write_pmf(f'{stem}_{i}{ext}', rmsd_pmfs[:,i], config, errors=None)
 
-    write_pmf(config.out_file, whole_pmf, config, errs)
-else:
-    write_pmf(config.out_file, whole_pmf, config, errors=None)
+    # Write whole PMF
+    if config.Nintervals>1:
+        # Compute errors in each point
+        errs = np.zeros(config.Nbin)
+        for i in range(config.Nbin):    
+            errs[i] = np.std(rmsd_pmfs[i,:])
+
+        write_pmf(config.out_file, whole_pmf, config, errs)
+    else:
+        write_pmf(config.out_file, whole_pmf, config, errors=None)
